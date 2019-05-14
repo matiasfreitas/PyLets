025baf312e8b49641fb112210b5c5c951bff46d9
matiasfreitasguimaraes <matiasfreitasguimares@gmail.com>
2019-05-14 12:37:19 -0300

    Change Setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['PyLets'],
     # Needed for dependencies
-    install_requires=['numpy', "math","scipy","matplotlib", "plotly"],
+    install_requires=['numpy',"scipy","matplotlib", "plotly"],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
