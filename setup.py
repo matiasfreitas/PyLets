09160b9946f64f9d521adb93f2268f687b175c89
matiasfreitasguimaraes <matiasfreitasguimares@gmail.com>
2019-05-14 14:47:10 -0300

    Version 3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy',"scipy","matplotlib", "plotly"],
     # *strongly* suggested for sharing
-    version='0.2',
+    version='0.3',
     # The license can be anything you like
     license='MIT',
     description='PyLets: Functions for wavelet Analysis',
